c64ee1611f1dd8c32a6c401c2c2b494fcbd1e551
Hector Zambrino <69545003+brino1997@users.noreply.github.com>
2022-12-05 19:49:51 -0300

    Add files via upload

---
+++ unidad2.js
@@ -0,0 +1,14 @@
+var nombre = "Hector";
+let edad  = 25;
+var Desarrollador = true;
+const nacimiento = new Date("6,24,1997,");
+
+const libroFav = {
+    titulo:"mi planta de naranja lima",
+    autor:"José Mauro de Vasconcelos",
+    fecha:1968,
+    url:"https://www.isl.cl/wp-content/uploads/2017/08/Mi-planta-naranja-lima-.pdf",
+}
+
+const lista = [nombre, edad, Desarrollador, nacimiento, libroFav]
+console.log(lista);
