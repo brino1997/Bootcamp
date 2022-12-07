eb82f116b9c85628f1e66f707ee2503494f0271c
Hector Zambrino <69545003+brino1997@users.noreply.github.com>
2022-12-06 21:47:36 -0300

    Add files via upload

---
+++ 05-numeroJs.js
@@ -0,0 +1,23 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+let altura = 194;
+console.log(altura)
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let altura2 = 1.94
+console.log(altura2)
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 84.7;
+console.log(peso)
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let alturaRedondeada = Math.round(altura2)
+console.log(alturaRedondeada)
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeada = Math.floor(peso)
+console.log(pesoRedondeada)
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
